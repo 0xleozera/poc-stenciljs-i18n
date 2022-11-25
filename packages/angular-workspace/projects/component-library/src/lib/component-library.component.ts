742f513f34529315a087faed870d9884d9b00eea
Leonardo Pinheiro <leoprocha100@gmail.com>
2022-11-25 00:04:29 -0300

    chore(angular): start project

---
+++ packages/angular-workspace/projects/component-library/src/lib/component-library.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-component-library',
+  template: `
+    <p>
+      component-library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ComponentLibraryComponent {
+
+}
